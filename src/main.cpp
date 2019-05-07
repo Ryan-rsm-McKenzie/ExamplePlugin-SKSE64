6d200d8830432b737cf0927d941f67ed7f28b138
Ryan-rsm-McKenzie <ryan__mckenzie@hotmail.com>
2019-05-06 18:10:17 -0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+﻿#include "common/IDebugLog.h"  // IDebugLog
+#include "skse64_common/skse_version.h"  // RUNTIME_VERSION
+#include "skse64/PluginAPI.h"  // SKSEInterface, PluginInfo
+
+#include <ShlObj.h>  // CSIDL_MYDOCUMENTS
+
+#include "version.h"  // VERSION_VERSTRING, VERSION_MAJOR
+
+
+extern "C" {
+	bool SKSEPlugin_Query(const SKSEInterface* a_skse, PluginInfo* a_info)
+	{
+		gLog.OpenRelative(CSIDL_MYDOCUMENTS, "\\My Games\\Skyrim Special Edition\\SKSE\\MyFirstPlugin.log");
+		gLog.SetPrintLevel(IDebugLog::kLevel_DebugMessage);
+		gLog.SetLogLevel(IDebugLog::kLevel_DebugMessage);
+
+		_MESSAGE("MyFirstPlugin v%s", MYFP_VERSION_VERSTRING);
+
+		a_info->infoVersion = PluginInfo::kInfoVersion;
+		a_info->name = "MyFirstPlugin";
+		a_info->version = MYFP_VERSION_MAJOR;
+
+		if (a_skse->isEditor) {
+			_FATALERROR("[FATAL ERROR] Loaded in editor, marking as incompatible!\n");
+			return false;
+		} else if (a_skse->runtimeVersion != RUNTIME_VERSION_1_5_73) {
+			_FATALERROR("[FATAL ERROR] Unsupported runtime version %08X!\n", a_skse->runtimeVersion);
+			return false;
+		}
+
+		return true;
+	}
+
+
+	bool SKSEPlugin_Load(const SKSEInterface* a_skse)
+	{
+		_MESSAGE("[MESSAGE] MyFirstPlugin loaded");
+
+		return true;
+	}
+};
